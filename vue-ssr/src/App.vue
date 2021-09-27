cb5d9b3942a648b45c40b2866a82fd400e409407
tianxiang <2546291626@qq.com>
2021-09-27 14:41:53 +0800

    vue自定义实现服务端渲染

---
+++ vue-ssr/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <h1>{{meassge}}</h1>
+        <div>
+            <el-input type="text" v-model="meassge" placeholder=""></el-input>
+        </div>
+        <div>
+            <button @click="click">点击</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+           meassge: 'hello world'
+        };
+    },
+    methods: {
+        click() {
+            console.log(this.meassge);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

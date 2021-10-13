ab475931866f3ad1fa5b14de4a9f015fddf2474b
tianxiang <2546291626@qq.com>
2021-10-13 16:10:45 +0800

    节流函数调整

--- interview/test/detail.js
+++ interview/test/detail.js
@@ -11,9 +11,9 @@ function debounce(fn, wait = 500) {
 
 //节流
 function throttle(fn, wait = 500) {
-    let now = Date.now();
     let old = 0;
     return function() {
+        let now = Date.now();
         if (now - old > wait) {
             fn()
             old = now;

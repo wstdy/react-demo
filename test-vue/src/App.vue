60ba7193e676331cfa2a8cb3fb5fc801d629914b
wstdy <2546291626@qq.com>
2021-11-01 17:14:00 +0800

    vue相关测试

---
+++ test-vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <hello-world :list="list" :age="age"></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      list: [
+        {id: 1, name: '刚子'},
+        {id: 2, name: '梁刚'}
+      ],
+      age: '123'
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
